02e8d74f8fd4b6ea3d8edc7fd8c1fe4f06f124b9
shubham0182 <thakorshubham548@gmail.com>
2026-06-04 21:17:29 +0530

    Fix service worker URLs for GitHub Pages subdirectory & improve Google login error message

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='ts-store-v1';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','index.html','manifest.json'])).then(()=>self.skipWaiting()))});
+const CACHE='ts-store-v2';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.map(n=>n!==CACHE?caches.delete(n):Promise.resolve()))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{const r=e.request;e.respondWith(caches.match(r).then(c=>c||fetch(r).then(res=>{if(res&&res.status===200){const ca=caches.open(CACHE);ca.then(c=>c.put(r,res.clone()))}return res}).catch(()=>new Response('Offline',{status:503}))))});
+self.addEventListener('fetch',e=>{const r=e.request;if(r.method!='GET')return;e.respondWith(caches.match(r).then(c=>c||fetch(r).then(res=>{if(res&&res.status===200){const ca=caches.open(CACHE);ca.then(c=>c.put(r,res.clone()))}return res}).catch(()=>new Response('Offline',{status:503}))))});
